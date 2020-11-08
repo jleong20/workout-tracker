aef17a664183e0b5a47e49e8a9bf5c71d5d3322e
Jerry Leong <jerry.leong20@gmail.com>
2020-11-07 23:39:31 -0800

    add api routes for workout & update readme

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -13,7 +14,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{
     useFindAndModify: false
 });
 
-//app.use(require("./public/api.js"));
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+app.get("/exercise", function(req, res){
+    res.sendFile(path.join(__dirname + '/public/exercise.html'));
+})
+app.get("/stats", function(req, res){
+    res.sendFile(path.join(__dirname + '/public/stats.html'));
+})
 
 app.listen(PORT, ()=>{
     console.log(`app running in port ${PORT}`);
